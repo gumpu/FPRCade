dc26e41cb200cd78fa6e959b7f890bd21b80ba44
Frans Slothouber <frans.slothouber@intraffic.nl>
2024-10-27 09:17:59 +0100

    Prepare for assembler testcases

---
+++ SubProjects/FAsm/src/Test/test_001.asm
@@ -0,0 +1,27 @@
+; Test
+;
+    ; Space should be skipped
+    ; Also for directives
+
+    .str "test"
+
+; Label inside a string
+.str "label1:"
+
+.code
+    nop
+    ldl d 0x0000
+label1:
+    ldl d 0x0001
+    add d
+    dup d
+    ldl d 0x000A
+    gt
+    bif label1
+    halt
+end:
+
+; Remark inside a string
+.str "; should not be lost"
+
+; --------------- end of file ----------------------
